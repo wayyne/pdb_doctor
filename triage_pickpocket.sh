ccf20c8a1f577e9b4c0669045c2cb797bcc347d9
Wayyne <iwayyne@live.com>
2025-02-06 18:07:55 -0500

    refined for application on pp direct output

--- triage_pickpocket.sh
+++ triage_pickpocket.sh
@@ -2,18 +2,44 @@
 
 basecamp=/home/wayyne/wrk/pdb_doctor
 
-python ${basecamp}/extract_sequence.py .
+#get list of complete and incomplete pdbs
+python ${basecamp}/extract_sequence.py . _pp.pdb && \
 
+#run the incomplete pdbs through fold and transfer
 awk -v basecamp=${basecamp} ' 
 NR > 1 {
-  print " python "basecamp"/fold_and_transfer.py",$1,$2,substr($1,1,8)"D.pdb"
+  print " python "basecamp"/fold_and_transfer.py",$1,$2,substr($1,1,5)"triaged.pdb"
 }
-' incomplete.tsv | bash
+' incomplete.tsv | bash && \
 
-mkdir input
-mv *_D.pdb input/.
+#copy the complete pdbs (do not need to fold or transfer)
+awk -v basecamp=${basecamp} ' 
+NR > 1 {
+  print "cp "$1,substr($1,1,5)"triaged.pdb"
+}
+' complete.tsv | bash && \
 
-time python ${basecamp}/complete_pdb.py --mode partial --input input --output output
+#collect the 'triaged' pdbs
+mkdir -p incomplete_triaged_pdbs && mv *_triaged.pdb incomplete_triaged_pdbs/. && \
 
-#update contact labels with sticky fingers
-time python ${basecamp}/sticky_fingers.py
+#run the 'triaded' pdbs throug complete PDB to add missing atoms and optimize newly added side-chains
+time python ${basecamp}/complete_pdb.py --mode partial --input incomplete_triaged_pdbs --output _triaged_pdbs && \
+
+#confirm/refine contact labels with sticky fingers
+time python ${basecamp}/sticky_fingers.py _triaged_pdbs && \
+
+#oraganize the complete triaged (aka chimeric) PDBs
+cd _triaged_pdbs
+mkdir series_C && mv *_C.pdb series_C/.
+
+#housekeeping
+rm -rf *_A.pdb *_B.pdb
+mv ../*.tsv .
+mv ../*.log .
+mv ../sticky*txt .
+
+#return to parent directory
+cd -
+
+#finish housekeeping
+rm -rf incomplete_triaged_pdbs pdb_cache fitted_folded.pdb
