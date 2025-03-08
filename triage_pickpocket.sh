956d261b55ebedafbf88986af19eeedabf567dc9
Wayyne <iwayyne@live.com>
2025-03-08 01:01:34 -0500

    updt

--- triage_pickpocket.sh
+++ triage_pickpocket.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-basecamp=/home/wayyne/wrk/pdb_doctor
+basecamp=/home/wayyne/wrk/pdb_doctor/scripts
+
+#strip ligands from all PDBs
+for pdb in *_pp.pdb; do
+  awk '$1 == "ATOM" {print $0}' $pdb > ${pdb::4}_pp_stripped.pdb
+done
 
 #get list of complete and incomplete pdbs
-python ${basecamp}/extract_sequence.py . _pp.pdb && \
+python ${basecamp}/extract_sequence.py . _pp_stripped.pdb && \
 
 #run the incomplete pdbs through fold and transfer
 awk -v basecamp=${basecamp} ' 
@@ -12,10 +17,10 @@ NR > 1 {
 }
 ' incomplete.tsv | bash && \
 
-#copy the complete pdbs (do not need to fold or transfer)
+#copy the complete pdbs w/o HETATMs (do not need to fold or transfer)
 awk -v basecamp=${basecamp} ' 
 NR > 1 {
-  print "cp "$1,substr($1,1,5)"triaged.pdb"
+  print "grep -v 'HETATM'",$1,">",substr($1,1,5)"triaged.pdb"
 }
 ' complete.tsv | bash && \
 
@@ -38,8 +43,15 @@ mv ../*.tsv .
 mv ../*.log .
 mv ../sticky*txt .
 
+mkdir triage_log
+mv *.txt triage_log
+mv *.log triage_log
+mv *.dat triage_log
+
 #return to parent directory
 cd -
 
 #finish housekeeping
-rm -rf incomplete_triaged_pdbs pdb_cache fitted_folded.pdb
+rm -rf incomplete_triaged_pdbs pdb_cache fitted_folded.pdb *_pp_stripped.pdb
+
+
